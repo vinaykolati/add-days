83c1e29e59f4086993a3e45e85f4f61ba8c5ad70
vinay <vinaukolati4995@gmail.com>
2022-07-23 14:07:30 +0000

    used arrow function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const addDays = require("date-fns/addDays");
 
-const getDateXDaysAfter = (days) = {
-    const newDate = addDays(new Date(2020,7,22),days);
-    return `${newDate.getDate()} - ${newDate.getMonth() + 1} - ${newDate.getFullYear()}`;
+const getDateXDaysAfter = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()} - ${
+    newDate.getMonth() + 1
+  } - ${newDate.getFullYear()}`;
 };
 
 module.exports = getDateXDaysAfter;
